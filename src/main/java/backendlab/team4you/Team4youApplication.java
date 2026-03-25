d5b0526cca40ea2ec3cbe9ed16f2114a7029d881
JohanHiths <johan.hietala@iths.se>
2026-03-25 16:19:59 +0100

    added docker, pomxml

---
+++ src/main/java/backendlab/team4you/Team4youApplication.java
@@ -0,0 +1,13 @@
+package backendlab.team4you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team4youApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team4youApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backendlab/team4you/Team4youApplicationTests.java
@@ -0,0 +1,13 @@
+package backendlab.team4you;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team4youApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
