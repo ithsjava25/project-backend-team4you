3ce3d4f05fbfecd784c86dd8d46d950efa1c0499
JohanHiths <johan.hietala@iths.se>
2026-04-01 21:04:52 +0200

    update

--- src/main/java/backendlab/team4you/repository/UserRepository.java
+++ src/main/java/backendlab/team4you/repository/UserRepository.java
@@ -5,6 +5,9 @@ import backendlab.team4you.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
+    Optional<UserEntity> findByEmail(String email);
 }
