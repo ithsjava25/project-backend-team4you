9d8e0afa106ffa24cd92866eec7a75c9a8d4aae6
JohanHiths <johan.hietala@iths.se>
2026-04-01 23:43:56 +0200

    Exceptions, mapper

---
+++ src/main/java/backendlab/team4you/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package backendlab.team4you.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND) // 404
+    public String notFoundException(UserNotFoundException ex, Model model) {
+
+        model.addAttribute("errorMessage", ex.getMessage());
+
+        return "error";
+    }
+}
---
+++ src/main/java/backendlab/team4you/mapper/UserMapper.java
@@ -0,0 +1,34 @@
+package backendlab.team4you.mapper;
+
+import backendlab.team4you.dto.UserDTO;
+import backendlab.team4you.user.UserEntity;
+
+public class UserMapper {
+
+
+//    public static UserEntity toEntity(UserRegistrationDto dto) {
+//        if (dto == null) return null;
+//
+//        UserEntity entity = new UserEntity();
+//        entity.setFirstName(dto.getFirstName());
+//        entity.setLastName(dto.getLastName());
+//        entity.setEmail(dto.getEmail());
+//        entity.setPhoneNumber(dto.getPhoneNumber());
+//
+//        return entity;
+//    }
+//
+//
+//    public static UserDTO toDto(UserEntity entity) {
+//        if (entity == null) return null;
+//
+//        UserDTO dto = new UserDTO();
+//        dto.setId(entity.getId());
+//        dto.setFirstName(entity.getFirstName());
+//        dto.setLastName(entity.getLastName());
+//        dto.setEmail(entity.getEmail());
+//        dto.setFullName(entity.getFirstName() + " " + entity.getLastName());
+//        return dto;
+//    }
+}
+
