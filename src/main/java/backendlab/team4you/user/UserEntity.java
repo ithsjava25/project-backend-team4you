c10b8b3d36510e3c508bdae86e0e0aeed7b3b0e0
JohanHiths <johan.hietala@iths.se>
2026-04-02 21:24:08 +0200

    Added fiels to UserEntity

--- src/main/java/backendlab/team4you/user/UserEntity.java
+++ src/main/java/backendlab/team4you/user/UserEntity.java
@@ -18,24 +18,30 @@ public class UserEntity implements PublicKeyCredentialUserEntity {
     @Column(name = "id", length = 255)
     private String id;
 
-    @Column(unique = true, nullable = false)
-    private String name;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
 
-    private String displayName;
+    @Column(name = "first_name")
+    private String firstName;
 
-    @Column(name = "password_hash", nullable = true)
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "password_hash")
     private String passwordHash;
 
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
 
     public UserEntity() {
     }
 
     public UserEntity(Bytes id, String name, String displayName) {
         this.id = id != null ? id.toBase64UrlString() : null;
-        this.name = name;
-        this.displayName = displayName;
+
         this.createdAt = LocalDateTime.now();
     }
 
@@ -50,17 +56,16 @@ public class UserEntity implements PublicKeyCredentialUserEntity {
 
     @Override
     public String getName() {
-        return name;
+        return email;
     }
+
+
 
     @Override
-    public @Nullable String getDisplayName() {
-        return displayName;
+    public String getDisplayName() {
+        return this.firstName + " " + this.lastName;
     }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
+    
 
     public LocalDateTime getCreatedAt() {
         return createdAt;
@@ -76,5 +81,37 @@ public class UserEntity implements PublicKeyCredentialUserEntity {
 
     public void setPasswordHash(String passwordHash) {
         this.passwordHash = passwordHash;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
