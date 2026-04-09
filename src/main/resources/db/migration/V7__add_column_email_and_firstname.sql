36be9e73bb0e68cca843011fec555ea2cc56d13b
Gabriela Aguirre <gvaguirres@gmail.com>
2026-04-09 17:19:30 +0200

    Change e-post for användarnamn. Add dev user. Add new migration

--- src/main/resources/db/migration/V7__add_column_email_and_firstname.sql
+++ src/main/resources/db/migration/V7__add_column_email_and_firstname.sql
@@ -1,3 +1,6 @@
+ALTER TABLE user_credentials RENAME COLUMN id TO credential_id;
+ALTER TABLE user_credentials RENAME COLUMN user_entity_id TO user_entity_user_id;
+
 ALTER TABLE user_entities
     ADD COLUMN IF NOT EXISTS email VARCHAR(255),
     ADD COLUMN IF NOT EXISTS first_name VARCHAR(255);
