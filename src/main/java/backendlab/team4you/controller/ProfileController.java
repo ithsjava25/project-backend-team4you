02649d40f11e316b0d15042d5997606c3f8638e4
JohanHiths <johan.hietala@iths.se>
2026-04-15 10:03:24 +0200

    Refactored admin dashboard templates and styles: integrated Thymeleaf layout fragments, updated CSS with reusable variables and classes, revised sidebar structure for role-based navigation, and improved consistency with updated layouts.

--- src/main/java/backendlab/team4you/controller/ProfileController.java
+++ src/main/java/backendlab/team4you/controller/ProfileController.java
@@ -1,15 +1,22 @@
 package backendlab.team4you.controller;
 
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProfileController {
 
     @GetMapping("/profile")
-    public String profile() {
-        return "fragments/profile :: content";
+    public String profile(Model model,
+                          @AuthenticationPrincipal UserDetails userDetails) {
+
+        model.addAttribute("username", userDetails.getUsername());
+
+        return "profile";
     }
 
 }
