52338b932d6c29e916ad864c78b4199fde305ce7
Rickard Ankar <rickard.ankar@iths.se>
2026-04-22 00:09:27 +0200

    test: add POST contact/send ContactControllerTest

--- src/test/java/backendlab/team4you/controller/ContactControllerTest.java
+++ src/test/java/backendlab/team4you/controller/ContactControllerTest.java
@@ -25,4 +25,15 @@ class ContactControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("contact"));
     }
+
+    @Test
+    void receiveMessage_shouldSaveAndReturnSuccess() throws Exception {
+        mockMvc.perform(post("/contact/send")
+                        .param("firstName", "Sven")
+                        .param("lastName", "Svensson")
+                        .param("email", "sven@test.com")
+                        .param("phone", "0701234567")
+                        .param("message", "Hello"))
+                .andExpect(status().isOk());
+    }
 }
