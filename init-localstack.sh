d994441fa1edd94310a984a7e52bcc8c8c54b4f4
Martin Stenhagen <martin.stenhagen@iths.se>
2026-04-21 10:19:23 +0200

    chaged init-localstack.sh to autocreate bucket etc

--- init-localstack.sh
+++ init-localstack.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -euo pipefail
+
 bucket="${AWS_BUCKET_NAME:-team4you-files}"
+echo "checking/creating s3 bucket: $bucket"
+
 awslocal s3api head-bucket --bucket "$bucket" 2>/dev/null || awslocal s3 mb "s3://$bucket"
+
+echo "available buckets:"
+awslocal s3 ls
