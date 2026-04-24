644146c98ae265edc65df15ec6b515119bc73c2a
Gabriela Aguirre <gvaguirres@gmail.com>
2026-04-24 22:08:06 +0200

    Remove System.out.println of authorities and add endpoint to close a case instead of delete it

--- src/main/java/backendlab/team4you/controller/CaseOfficerController.java
+++ src/main/java/backendlab/team4you/controller/CaseOfficerController.java
@@ -1,13 +1,16 @@
 package backendlab.team4you.controller;
 
-import backendlab.team4you.application.ApplicationService;
 import backendlab.team4you.caserecord.CaseRecord;
 import backendlab.team4you.caserecord.CaseRecordRepository;
+import backendlab.team4you.caserecord.CaseRecordService;
+import backendlab.team4you.caserecord.CaseStatus;
+import backendlab.team4you.exceptions.CaseRecordNotFoundException;
 import backendlab.team4you.exceptions.UserNotFoundException;
 import backendlab.team4you.user.UserEntity;
 import backendlab.team4you.user.UserRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -15,6 +18,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @PreAuthorize("hasRole('CASE_OFFICER')")
@@ -29,8 +34,7 @@ public class CaseOfficerController {
     }
 
     @GetMapping("/case-officer")
-    public String admin(Authentication auth) {
-        System.out.println(auth.getAuthorities());
+    public String caseOfficerHome() {
         return "case-officer";
     }
 
@@ -53,15 +57,22 @@ public class CaseOfficerController {
         return "fragments/case-officer-cases :: content";
     }
 
-    //todo: endpoint to delete a case
+    @PostMapping("/case-officer/cases/close")
+    @ResponseBody
+    public String closeCase(@RequestParam String id, Authentication auth) {
+        UserEntity officer = userRepository.findByName(auth.getName())
+                .orElseThrow(() -> new UserNotFoundException("Case Officer not found"));
 
-//    @PostMapping("/case-officer/cases/delete")
-//    public String deleteApplication(@RequestParam Long id, Model model) {
-//
-//        applicationService.delete(id);
-//
-//        model.addAttribute("message", "Ansökan borttagen");
-//
-//        return "fragments/alert :: success";
-//    }
+        CaseRecord caseRecord = caseRecordRepository.findById(Long.valueOf(id))
+                .orElseThrow(() -> new CaseRecordNotFoundException(id));
+
+        if (!caseRecord.getAssignedUser().getId().equals(officer.getId())) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Not assigned to this officer");
+        }
+
+        caseRecord.setStatus(CaseStatus.CLOSED);
+        caseRecordRepository.save(caseRecord);
+
+        return "";
+    }
 }
